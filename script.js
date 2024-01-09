5fe98191fd56ab6afa88d020daffd3a9a73275b0
Constantin <horghidanconstantin91@yahoo.com>
2024-01-09 16:27:38 +0000

    Starter code

---
+++ script.js
@@ -0,0 +1,135 @@
+// Array of special characters to be included in password
+var specialCharacters = [
+  '@',
+  '%',
+  '+',
+  '\\',
+  '/',
+  "'",
+  '!',
+  '#',
+  '$',
+  '^',
+  '?',
+  ':',
+  ',',
+  ')',
+  '(',
+  '}',
+  '{',
+  ']',
+  '[',
+  '~',
+  '-',
+  '_',
+  '.'
+];
+
+// Array of numeric characters to be included in password
+var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+// Array of lowercase characters to be included in password
+var lowerCasedCharacters = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z'
+];
+
+// Array of uppercase characters to be included in password
+var upperCasedCharacters = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z'
+];
+
+// Function to prompt user for password options
+function getPasswordOptions() {
+
+}
+
+// Function for getting a random element from an array
+function getRandom(arr) {
+
+}
+
+// Function to generate password with user input
+function generatePassword() {
+      //1. Prompt the user for the password criteria
+      //    a. Password length 8<128
+      //    b. Lowercase , uppercase , numbers, special characters
+      //2. Validate the imput
+      //3. Display password to the page 
+  let characterLength = parseInt(prompt("How many cgaracters? Must be at leas 8."));
+  if (characterLength < 8 || characterLength >128){
+    alert("Sorry,must be a value between 8 and 128.")
+    return
+  }
+  let characterTypeUpper = confirm("Would you like Uppercase characters?");
+  
+  let characterTypeLower = confirm("Would you like Lower Case characters?");
+  let characterTypeNum = confirm("Would you like any numbers?");
+  let characterTypeSpecial = confirm("Would you like Special characters?");
+    
+
+
+}
+
+// Get references to the #generate element
+var generateBtn = document.querySelector('#generate');
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector('#password');
+
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener('click', writePassword);
