9a535e706e74d22194917deb8b4d905349a4fc3b
Constantin <horghidanconstantin91@yahoo.com>
2024-01-09 21:57:15 +0000

    js prompt and validation edit

--- script.js
+++ script.js
@@ -90,7 +90,22 @@ var upperCasedCharacters = [
 
 // Function to prompt user for password options
 function getPasswordOptions() {
-
+// Ask users for password criteria
+let characterLength = parseInt(prompt("How many cgaracters? Must be at leas 8."));
+if (characterLength < 8 || characterLength >128){
+   alert("Sorry,must be a value between 8 and 128.")
+   return null;
+}
+  
+let characterTypeUpper = confirm("Would you like Uppercase characters?");
+let characterTypeLower = confirm("Would you like Lower Case characters?");
+let characterTypeNum = confirm("Would you like any numbers?");
+let characterTypeSpecial = confirm("Would you like Special characters?");
+}
+// Validate the user input
+if(!characterTypeUpper && !characterTypeLower && !characterTypeNum && !characterTypeSpecial) {
+  alert("You must select at least one character type.");
+  return null;
 }
 
 // Function for getting a random element from an array
@@ -100,22 +115,7 @@ function getRandom(arr) {
 
 // Function to generate password with user input
 function generatePassword() {
-      //1. Prompt the user for the password criteria
-      //    a. Password length 8<128
-      //    b. Lowercase , uppercase , numbers, special characters
-      //2. Validate the imput
-      //3. Display password to the page 
-  let characterLength = parseInt(prompt("How many cgaracters? Must be at leas 8."));
-  if (characterLength < 8 || characterLength >128){
-    alert("Sorry,must be a value between 8 and 128.")
-    return
-  }
-  let characterTypeUpper = confirm("Would you like Uppercase characters?");
-  
-  let characterTypeLower = confirm("Would you like Lower Case characters?");
-  let characterTypeNum = confirm("Would you like any numbers?");
-  let characterTypeSpecial = confirm("Would you like Special characters?");
-    
+     
 
 
 }
